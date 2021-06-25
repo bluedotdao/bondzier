e824fae45506c1aa90c5f57ce50baaef12e5c5cd
Johnny Pedala <johnny@pop-os.localdomain>
2021-06-25 22:01:34 +0200

    moar tests

--- test/Bondzier.js
+++ test/Bondzier.js
@@ -107,4 +107,149 @@ describe("Bondzier", function() {
         
     
     })
+    it("Owner must recieve sell proceeds", async function() {
+
+        const [deployer, acc2] = await ethers.getSigners();
+       
+        let timestamp = Math.round(Date.now() / 1000) + 900
+        let text = "Pozz!";
+        let bytes = ethers.utils.toUtf8Bytes(text);
+         let startPrice = 0
+         let endPrice = 3
+        let total = ethers.utils.parseUnits('3', 18)
+        let amnt = 1;
+        let salt = ethers.utils.formatBytes32String('NewBondzier2');
+        let bb = await bondzierFactory.createBondzier(true, amnt, total, [startPrice, 3, 0, 3, 0, endPrice], deployer.address, timestamp, "https://bondzier2.io", salt, bytes, {
+            gasLimit: 900000
+        })
+        let res = await bb.wait();
+        let newBondzierAddress = res.events[0].args.newBondzierAddress;
+         let balanceStart = await deployer.getBalance();
+        const _bondzier = new ethers.Contract(newBondzierAddress, Bondzier.interface, acc2);
+        let nextPrice = await _bondzier.nextPrice()
+        await _bondzier.buy({value : nextPrice})
+        nextPrice = await _bondzier.nextPrice()
+        await _bondzier.buy({value : nextPrice})
+        nextPrice = await _bondzier.nextPrice()
+        await _bondzier.buy({value : nextPrice})
+        let balanceEnd = await deployer.getBalance();
+        await expect(balanceEnd.sub(balanceStart).toString()).to.equal('3')
+
+        
+    
+    })
+    it("Buyer must recieve the token after buy", async function() {
+
+        let abi = [
+    "event TransferSingle(address indexed operator, address indexed from, address indexed to, uint256 id, uint256 value)"
+];
+
+let iface = new ethers.utils.Interface(abi)
+        const [deployer, acc2] = await ethers.getSigners();
+       
+        let timestamp = Math.round(Date.now() / 1000) + 900
+        let text = "Pozz!";
+        let bytes = ethers.utils.toUtf8Bytes(text);
+         let startPrice = 0
+         let endPrice = 3
+        let total = ethers.utils.parseUnits('3', 18)
+        let amnt = 1;
+        let salt = ethers.utils.formatBytes32String('NewBondzier3');
+        let bb = await bondzierFactory.createBondzier(true, amnt, total, [startPrice, 3, 0, 3, 0, endPrice], deployer.address, timestamp, "https://bondzier2.io", salt, bytes, {
+            gasLimit: 900000
+        })
+        let res = await bb.wait();
+        let newBondzierAddress = res.events[0].args.newBondzierAddress;
+         let balanceStart = await deployer.getBalance();
+        const _bondzier = new ethers.Contract(newBondzierAddress, Bondzier.interface, acc2);
+        let nextPrice = await _bondzier.nextPrice()
+        let buyTransaction = await _bondzier.buy({value : nextPrice})
+        let buyRes = await buyTransaction.wait()
+
+        
+        expect(iface.parseLog(buyRes.events[0]).args.to).to.equal(acc2.address)
+
+        expect(iface.parseLog(buyRes.events[0]).args.value.toString()).to.equal('1')        
+    
+    })
+
+
+    it("If the token is non-fungible, each buy should generate new token id", async function() {
+
+        let abi = [
+    "event TransferSingle(address indexed operator, address indexed from, address indexed to, uint256 id, uint256 value)"
+];
+
+let iface = new ethers.utils.Interface(abi)
+        const [deployer, acc2] = await ethers.getSigners();
+       
+        let timestamp = Math.round(Date.now() / 1000) + 900
+        let text = "Pozz!";
+        let bytes = ethers.utils.toUtf8Bytes(text);
+         let startPrice = 0
+         let endPrice = 3
+        let total = ethers.utils.parseUnits('3', 18)
+        let amnt = 1;
+        let salt = ethers.utils.formatBytes32String('NewBondzier4');
+        let bb = await bondzierFactory.createBondzier(true, amnt, total, [startPrice, 3, 0, 3, 0, endPrice], deployer.address, timestamp, "https://bondzier2.io", salt, bytes, {
+            gasLimit: 900000
+        })
+        let res = await bb.wait();
+        let newBondzierAddress = res.events[0].args.newBondzierAddress;
+         let balanceStart = await deployer.getBalance();
+        const _bondzier = new ethers.Contract(newBondzierAddress, Bondzier.interface, acc2);
+        let nextPrice = await _bondzier.nextPrice()
+        let buyTransaction = await _bondzier.buy({value : nextPrice})
+        let buyRes = await buyTransaction.wait()
+
+        let nextPrice2 = await _bondzier.nextPrice()
+        let buyTransaction2 = await _bondzier.buy({value : nextPrice})
+        let buyRes2 = await buyTransaction2.wait()
+        //console.log(buyRes.events[0])
+        //console.log(acc2.address)
+
+        expect(iface.parseLog(buyRes.events[0]).args.id.toString() !== iface.parseLog(buyRes2.events[0]).args.id.toString())
+        
+        
+    
+    })
+    it("If the token is fungible, each buy should generate same token id", async function() {
+
+        let abi = [
+    "event TransferSingle(address indexed operator, address indexed from, address indexed to, uint256 id, uint256 value)"
+];
+
+let iface = new ethers.utils.Interface(abi)
+        const [deployer, acc2] = await ethers.getSigners();
+       
+        let timestamp = Math.round(Date.now() / 1000) + 900
+        let text = "Pozz!";
+        let bytes = ethers.utils.toUtf8Bytes(text);
+         let startPrice = 0
+         let endPrice = 3
+        let total = ethers.utils.parseUnits('3', 18)
+        let amnt = 1;
+        let salt = ethers.utils.formatBytes32String('NewBondzier5');
+        let bb = await bondzierFactory.createBondzier(true, amnt, total, [startPrice, 3, 0, 3, 0, endPrice], deployer.address, timestamp, "https://bondzier2.io", salt, bytes, {
+            gasLimit: 900000
+        })
+        let res = await bb.wait();
+        let newBondzierAddress = res.events[0].args.newBondzierAddress;
+         let balanceStart = await deployer.getBalance();
+        const _bondzier = new ethers.Contract(newBondzierAddress, Bondzier.interface, acc2);
+        let nextPrice = await _bondzier.nextPrice()
+        let buyTransaction = await _bondzier.buy({value : nextPrice})
+        let buyRes = await buyTransaction.wait()
+
+        let nextPrice2 = await _bondzier.nextPrice()
+        let buyTransaction2 = await _bondzier.buy({value : nextPrice})
+        let buyRes2 = await buyTransaction2.wait()
+        //console.log(buyRes.events[0])
+        //console.log(acc2.address)
+
+        expect(iface.parseLog(buyRes.events[0]).args.id.toString() === iface.parseLog(buyRes2.events[0]).args.id.toString())
+        
+        
+    
+    })
 });
